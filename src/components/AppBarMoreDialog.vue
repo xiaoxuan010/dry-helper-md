f8d1b652c414d53f11f0f4afc4a3af1cc7d830fa
XUAN <148884605+xuan-nvsq@users.noreply.github.com>
2024-03-31 08:17:19 +0000

    update dialog

--- src/components/AppBarMoreDialog.vue
+++ src/components/AppBarMoreDialog.vue
@@ -6,5 +6,17 @@
 			<small>&nbsp;回南天数据洞察</small>
 		</h2>
 		<p>这里是“雾霭图谱——回南天数据洞察”的前端页面，本项目基于 Vue.js 等框架开发，在网页端实现类 App 的效果。</p>
+		<ul>
+			<li><a href="https://effective-eureka-699gwx4w6wxrc67q-5173.app.github.dev/#/map">全省天气观察</a> 
+				<small> &nbsp;省内各个地级市实时天象概况预览</small>
+			</li>
+			<li><a href="https://effective-eureka-699gwx4w6wxrc67q-5173.app.github.dev/#/overview">回南天气总览</a>
+				<small> &nbsp;广州市内24小时温度可视化</small>
+			</li>
+			<li><a href="https://effective-eureka-699gwx4w6wxrc67q-5173.app.github.dev/#/stats">回南天气统计</a>
+				<small> &nbsp;三年内省内回南天相关数据展示</small>
+			</li>
+		</ul>
 	</div>
+
 </template>
