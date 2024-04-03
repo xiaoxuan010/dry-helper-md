c5ea3c7cc1c6d1c9cabb01dd09a44bddbcce7b1a
azuser770 <148884605+azuser770@users.noreply.github.com>
2024-04-03 11:14:13 +0800

    update lw.message

--- src/components/DewTemIntro.vue
+++ src/components/DewTemIntro.vue
@@ -2,7 +2,16 @@
 	<div class="root">
 		<div class="mdui-prose">
 			<h2>露点地温差</h2>
-			<p><mdui-icon name="circle"></mdui-icon> 绿色：<= -1</p>
+			<div align="left">
+				<samll ><mdui-icon name="circle" style="font-size:10px; color: green;" ></mdui-icon> 绿色：<=-0.5 </samll>
+				<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small><strong>绿色表示该点无回南天</strong></small></span>
+				<br />
+				<samll ><mdui-icon name="circle" style="font-size:10px; color: orange;" ></mdui-icon> 橙色：-0.5~2 </samll>
+				<span>&nbsp;&nbsp;&nbsp;&nbsp;<small><strong>橙色表示该点出现回南天的可能性很大</strong></small></span>
+				<br />
+				<samll ><mdui-icon name="circle" style="font-size:10px; color: red;" ></mdui-icon> 红色：>2 </samll>
+				<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small><strong>红色表示该点出现严重的回南天</strong></small></span>
+			</div>
 		</div>
 	</div>
 </template>
@@ -12,3 +21,4 @@
 	text-align: left;
 }
 </style>
+·
